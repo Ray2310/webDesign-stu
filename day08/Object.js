a148424a780cf7761870343df7f9be49d6d58620
Rayce <1326507725@qq.com>
2023-08-07 22:48:59 +0800

    update

--- day08/Object.js
+++ day08/Object.js
@@ -11,3 +11,15 @@ let object1 = {
 console.log(object1)
 console.log(object1['user-name'])
 object1.fun()
+
+console.log("动态添加属性和方法")
+ // 声明一个空的对象（没有任何属性，也没有任何方法）
+ let user = {}
+ // 动态追加属性
+ user.name = '小明'
+ user['age'] = 18
+ 
+ // 动态添加方法
+ user.move = function () {
+   console.log('移动一点距离...')
+ }
