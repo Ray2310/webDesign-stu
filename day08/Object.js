fbcbfbe39a87d187b1b2515f7ac095d7719e8964
Rayce <1326507725@qq.com>
2023-08-08 21:59:11 +0800

    update

--- day08/Object.js
+++ day08/Object.js
@@ -23,3 +23,14 @@ console.log("动态添加属性和方法")
  user.move = function () {
    console.log('移动一点距离...')
  }
+
+let arr = [123,1231,121]
+ for (const key in arr) {
+    console.log("key == ", key) // key是数组下标的索引， 但是是字符串的'0' , '1'... 所以最好不要使用这个来遍历数组。 而是遍历对象
+ }
+
+ //todo 遍历对象的方法
+ for (const key in object1) {
+  //key ==>  'uname' , 'age', 'gender' ......
+    console.log(object1[key]) //可以打印出对应key的value
+  }
