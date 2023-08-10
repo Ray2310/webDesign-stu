96f701c0e8cd2d2f12c684f28f38d2528f86ff16
Rayce <1326507725@qq.com>
2023-08-10 22:43:34 +0800

    day9

--- day9/api-stu.js
+++ day9/api-stu.js
@@ -23,3 +23,10 @@ for(let i=0 ; i< rr.length;i++){
   console.log(rr[i].innerHTML)
   rr[i].style.color = 'red'
 }
+
+//获取输入框中的内容
+const username = document.querySelector('input')
+console.log(username.value) //username.innerHTML得不到表单的内容，只能通过username.value
+username.value = '我要买电脑' //设置表单的值
+username.type = 'password' //修改类型为password类型
+console.log(username.value)
